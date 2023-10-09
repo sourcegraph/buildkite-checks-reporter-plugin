03f8c7e31884344325f3b2e387869e3abb330eae
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-10-09 12:08:05 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "flake for bash buidlkite plugin development";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Add dependencies that are only needed for development
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          baseDeps = with pkgs; [ shellcheck];
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = baseDeps;
+            shellHook = ''
+            # Add stuff here if you want to run things as the shell becomes active
+            '';
+          };
+        });
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+    };
+}
